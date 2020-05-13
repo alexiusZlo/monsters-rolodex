d66c17a89a0325caef1b1a3a6f8806bce4df0dcf
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-13 13:36:43 +0300

    created search-box components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 
 import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from "./components/seach-box/seach-box.components";
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: ''
         }
     }
 
@@ -18,9 +20,17 @@ class App extends Component {
     }
 
     render() {
+        const {monsters, searchField} = this.state;
+        const filterMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
         return (
             <div className="App">
-                <CardList monsters={this.state.monsters}> </CardList>
+                <SearchBox
+                    placeholder='search monsters'
+                    handleChange={e => this.setState({searchField: e.target.value})}
+                />
+                <CardList monsters={filterMonsters}> </CardList>
             </div>
         )
     }
