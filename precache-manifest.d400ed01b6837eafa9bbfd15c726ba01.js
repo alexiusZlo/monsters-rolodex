348b734b35dd92291d8a11e5eec533eaa6e9c1b9
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-13 15:51:17 +0300

    Updates

---
+++ precache-manifest.d400ed01b6837eafa9bbfd15c726ba01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0a9d29b730a2e85e0a62aca85e5a25",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "22c73cf2a9010d621f30",
+    "url": "/monsters-rolodex/static/css/main.8253d5a5.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c73cf2a9010d621f30",
+    "url": "/monsters-rolodex/static/js/main.d9135e30.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
